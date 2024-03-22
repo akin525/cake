cc17095038865f4e5fe132cee3226b41a14c8b64
DELL <akinlabisamson15@gmail.com>
2024-03-22 13:35:22 +0100

    cheking

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-12">
                     <div class="breadcrumb_content">
                         <h1 class="breadcrumb_title"><b>ABOUT US</b></h1>
-                        <p class="breadcrumb_text">Welcome to Eko Cakes WHO WE ARE</p>
+{{--                        <p class="breadcrumb_text">Welcome to Eko Cakes WHO WE ARE</p>--}}
                     </div>
                 </div>
             </div>
@@ -24,8 +24,8 @@
             <div class="row align-items-center">
                 <div class="col-md-6">
                     <div class="section-title-10 justify-content-start text-start align-items-start">
-                        <span class="section-title-10__subtitle">We Are EkoCake</span>
-{{--                        <h1 class="section-title-10__title">we love cake</h1>--}}
+                        <span class="section-title-10__subtitle" style="font-family: Great Vibes, cursive;">Welcome to Eko Cakes</span>
+                        <h4 class="section-title-10__title">WHO WE ARE</h4>
                         <p class="section-title-10__text">{!! $setting->about !!}</p>
 {{--                        <p class="section-title-10__text">Vestibulum eu tristique tellus. Praesent at varius nisi, ut dignissim lectus. Praesent venenatis ipsum in arcu ullamcorper tristique. Aliquam et gravida magna, ut tincidunt massa.</p>--}}
 {{--                        <img src="assets/images/singnecher-2.png" alt="Signature-Image">--}}
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/eko.png')}}">
-
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <!-- CSS (Font, Vendor, Icon, Plugins & Style CSS files) -->
 
     <link rel="preconnect" href="https://fonts.googleapis.com/">
