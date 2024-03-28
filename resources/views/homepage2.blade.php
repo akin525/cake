30f762cbdc75a0e69b20d94ce7200dfc35dd92ad
DELL <akinlabisamson15@gmail.com>
2024-03-28 13:06:47 +0100

    cheking

--- resources/views/homepage2.blade.php
+++ resources/views/homepage2.blade.php
@@ -105,10 +105,10 @@
     </div>
 
     <br/>
-    <h5 class="section-title-10__subtitle text-center" style="color: black; font-weight: 200; font-size: 35px"><b>We know that making cake is art</b></h5>
+    <h5 class="section-title-10__subtitle text-center" style="color: black; font-weight: 200; font-size: 38px"><b>We know that making cake is art</b></h5>
 {{--    <h3 class="text-center section-title-10__subtitle" style="color: #eda939">Making cake is art</h3>--}}
     <hr style="font-weight: 300;"/>
-    <h6 class="text-center cormorant-upright-regular " style="color: #eda939; font-size: 35px" ><b>PLEASE VIEW OUR CATEGORIES</b></h6>
+    <h6 class="text-center cormorant-upright-regular " style="color: #eda939; " ><b>PLEASE VIEW OUR CATEGORIES</b></h6>
 {{--    <h5  style="font-family: Great Vibes, cursive;--}}
 {{--    font-weight: 400;--}}
 {{--    font-style: normal;">PLEASE VIEW OUR PRODUCTS BELOW--}}
