c37af5ea9dbc8f8888fd7b8b93ce594c2491f66b
DELL <akinlabisamson15@gmail.com>
2024-04-05 12:13:00 +0100

    layer and size

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -526,13 +526,13 @@
     <div class="footer-widget-section">
         <div class="container custom-container">
             <div class="row gy-6">
-                <div class="col-md-4">
+                <div class="text-center">
                     <!-- Footer Widget Section Strat -->
                     <div class="footer-widget">
                         <div class="">
                             <a href="/"><img width="50" src="{{asset('assets/images/eko.png')}}" alt="Logo"></a>
                         </div>
-                        <div class="footer-widget__social">
+                        <div class="">
                             <a href="#"><i class="lastudioicon-b-facebook"></i></a>
                             <a href="#"><i class="lastudioicon-b-twitter"></i></a>
                             <a href="#"><i class="lastudioicon-b-pinterest"></i></a>
@@ -540,52 +540,6 @@
                         </div>
                     </div>
                     <!-- Footer Widget Section End -->
-                </div>
-                <div class="col-md-8">
-                    <!-- Footer Widget Wrapper Strat -->
-                    <div class="footer-widget-wrapper d-flex flex-wrap gap-4">
-
-                        <!-- Footer Widget Strat -->
-                        <div class="footer-widget flex-grow-1">
-                            <h4 class="footer-widget__title sam" style="font-size: 15px">Categories</h4>
-
-                            <ul class="footer-widget__link cormorant-upright-bold">
-                                <li><a href="#">Cupcake</a></li>
-                                <li><a href="#">Pastry</a></li>
-                                <li><a href="#">Muffin</a></li>
-                                <li><a href="#">Waffle</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Widget End -->
-
-                        <!-- Footer Widget Strat -->
-                        <div class="footer-widget flex-grow-1">
-                            <h4 class="footer-widget__title sam" style="font-size: 15px">Services</h4>
-
-                            <ul class="footer-widget__link cormorant-upright-bold">
-                                <li><a href="contact.html">Delivery</a></li>
-                                <li><a href="contact.html">Payment</a></li>
-                                <li><a href="contact.html">Returns</a></li>
-                                <li><a href="contact.html">Privacy</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Widget End -->
-
-                        <!-- Footer Widget Strat -->
-                        <div class="footer-widget flex-grow-1">
-                            <h4 class="footer-widget__title sam" style="font-size: 15px">Information</h4>
-
-                            <ul class="footer-widget__link cormorant-upright-bold">
-                                <li><a href="about.html">About Us</a></li>
-                                <li><a href="contact.html">Contact Us</a></li>
-                                <li><a href="blog-details.html">Latest Post</a></li>
-                                <li><a href="about.html">Selling Tips</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Widget End -->
-
-                    </div>
-                    <!-- Footer Widget Wrapper End -->
                 </div>
             </div>
         </div>
