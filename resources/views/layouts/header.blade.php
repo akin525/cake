719e2968a8cc8cc77a33e31123ab41c46aadf36e
DELL <akinlabisamson15@gmail.com>
2024-04-23 20:32:44 +0100

    layer and size

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -562,7 +562,19 @@
 <!-- Footer End -->
 </div>
 <!-- JS Vendor, Plugins & Activation Script Files -->
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6628014ba0c6737bd12f79e7/1hs64a5vm';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <!-- Vendors JS -->
 <script src="{{asset('assets/js/vendor/modernizr-3.11.7.min.js')}}"></script>
 <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
