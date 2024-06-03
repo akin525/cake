d50fac7cf5a1ea34ee440523008d4b65e9732d65
DELL <akinlabisamson15@gmail.com>
2024-06-03 13:06:05 +0100

    layer and size

--- resources/views/shop/cakedetails.blade.php
+++ resources/views/shop/cakedetails.blade.php
@@ -405,6 +405,8 @@
                                         });
                                     });
                                 </script>
+                            <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+                            <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
                             <div class="product-size">
                                 <label for="layersBy" class="cormorant-upright-bold" ><span>Base Colour of Cake</span></label>
