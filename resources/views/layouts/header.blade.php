e5681cea31cdd23d155cfee3e5135a40d3aca945
DELL <akinlabisamson15@gmail.com>
2024-07-20 08:45:50 +0100

    checkout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -618,7 +618,30 @@
         </div>
     </div>
     <!-- Footer Widget Section End -->
+    <style>
+        .float{
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#25d366;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            font-size:30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index:100;
+        }
 
+        .my-float{
+            margin-top:16px;
+        }
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="https://wa.me/23480162300183" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
     <!-- Footer Copyright Strat -->
     <div class="footer-copyright">
         <div class="container">
