f024747136f09a31c88008a122360a63f62888d2
akina <akinlabisamson15@gmail.com>
2024-10-26 07:45:21 +0100

    bb

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -94,11 +94,11 @@
             <!-- Sidebar toggle button-->
             <div class="app-menu">
                 <ul class="header-megamenu nav">
-{{--                    <li class="btn-group nav-item d-md-none">--}}
-{{--                        <a href="#" class="waves-effect waves-light nav-link push-btn" data-toggle="push-menu" role="button">--}}
-{{--                            <span class="icon-Align-left"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
+                    <li class="btn-group nav-item d-md-none">
+                        <a href="#" class="waves-effect waves-light nav-link push-btn" data-toggle="push-menu" role="button">
+                            <span class="icon-Align-left"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
+                        </a>
+                    </li>
 {{--                    <li class="btn-group nav-item d-none d-xl-inline-block">--}}
 {{--                        <a href="#" class="waves-effect waves-light nav-link svg-bt-icon" title="Chat">--}}
 {{--                            <i class="icon-Chat"><span class="path1"></span><span class="path2"></span></i>--}}
